87bc2b3063ad0eaa84de4c6dc000657637e327ba
ReloadingBee <118016583+ReloadingBee@users.noreply.github.com>
2023-10-12 17:50:57 +0300

    Updates
    Added a whole bunch of updates...

---
+++ Assets/Scripts/Bird.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class Bird : MonoBehaviour
+{
+    public float jumpSpeed;
+    public int score = 0;
+    public TMP_Text scoreText;
+    public AudioSource scoreSound;
+    private readonly float rotatePower = 3;
+    Rigidbody2D rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            rb.velocity = Vector2.up * jumpSpeed;
+        }
+
+        transform.eulerAngles = new Vector3(0, 0, rb.velocity.y * rotatePower);
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        var currentScene = SceneManager.GetActiveScene().name;
+        SceneManager.LoadScene(currentScene);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        score++;
+        scoreText.SetText(score.ToString());
+        scoreSound.Play();
+    }
+}
---
+++ Assets/Scripts/Pipe.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class Pipe : MonoBehaviour
+{
+    public float speed;
+    public float startX;
+    public float endX;
+
+    private void Start()
+    {
+        startX = Camera.main.ViewportToWorldPoint(new Vector3(0, 0)).x - 1;
+        endX = Camera.main.ViewportToWorldPoint(new Vector3(1, 0)).x + 1;
+    }
+
+    private void Update()
+    {
+        transform.position += Vector3.left * speed * Time.deltaTime;
+
+        if(transform.position.x < startX)
+        {
+            var height = Random.Range(-2.5f, 3.5f);
+            transform.position = new Vector3(endX, height);
+        }
+    }
+}
